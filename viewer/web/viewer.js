ecaabbd9c3705d1f60b0d0e2939bd7b78c93bf5b
HongS <41907842+HongS1013@users.noreply.github.com>
2023-09-05 14:44:38 -0500

    Update viewer.js

--- viewer/web/viewer.js
+++ viewer/web/viewer.js
@@ -4119,7 +4119,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: 'intersection_disciplinaries.pdf',
+    value: 'Krumm-2018-ch2.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
