c8affcb3f8c33329ce4be6c21fe3bca426eb40d1
HongS <41907842+HongS1013@users.noreply.github.com>
2023-09-05 15:03:26 -0500

    Update viewer.js

--- viewer/web/viewer.js
+++ viewer/web/viewer.js
@@ -4119,7 +4119,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: 'Hidden_curriculum_text.pdf',
+    value: 'siemens-2013-learning-analytics-the-emergence-of-a-discipline.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
